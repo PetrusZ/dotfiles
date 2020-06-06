65b85744a744295092cf2fafca3cb9e4bc209968
Petrus.Z <silencly07@gmail.com>
2020-06-06 14:00:03 +0800

    weekly update by crontab script
    Signed-off-by: Petrus.Z <silencly07@gmail.com>

--- home/petrus/.doom.d/config.el
+++ home/petrus/.doom.d/config.el
@@ -38,6 +38,7 @@
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
 ;; - `load!' for loading external *.el files relative to this one
+
 ;; - `use-package' for configuring packages
 ;; - `after!' for running code after a package has loaded
 ;; - `add-load-path!' for adding directories to the `load-path', relative to ;;   this file. Emacs searches the `load-path' when you load packages with
@@ -93,6 +94,8 @@ recommended
   :type 'float)
 
 (map! :ne "<f3>" 'neotree-toggle)
+(map! :i "C-g" 'evil-normal-state)
+(map! :i "C-h" 'evil-delete-backward-char-and-join)
 
 ; (define-key evil-normal-state-map (kbd "<f3>") 'neotree-toggle)
 ; (global-set-key (kbd "<f1>") 'func)
